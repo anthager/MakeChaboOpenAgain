a51f61c6560240e2292ec956c1718cf0ea2774cf
anthager <anthage@student.chalmers.se>
2019-03-19 22:04:05 +0100

    cleaned up

--- handler.js
+++ handler.js
@@ -2,14 +2,15 @@
 // eslint-disable-next-line no-unused-vars
 const { unlockDoor } = require('./src')
 const aws = require('aws-sdk')
+// const { token } = require('./secrets/token')
 
+// const unlockDoor = async () => 'door unlocked, note that it just unlocks and dont open'
 // eslint-disable-next-line no-unused-vars
 const unlock = async (event, context) => {
-  // const res = await unlockDoor()
   const dynamoDb = new aws.DynamoDB.DocumentClient()
   const TableName = process.env.TABLE_NAME
-  const waitMS = 10000
-  const res = await dynamoDb
+  const waitMS = 8000
+  const query = await dynamoDb
     .query({
       TableName,
       Limit: 1,
@@ -23,25 +24,18 @@ const unlock = async (event, context) => {
       },
     })
     .promise()
-  const { timestamp } = res && res.Items[0]
-  const body =
-    !timestamp || +new Date() - timestamp > waitMS
-      ? {
-          success: 'true',
-        }
-      : {
-          success: 'false',
-          reason: 'recently opened',
-          wait: waitMS - (+new Date() - timestamp),
-        }
-
+  const { timestamp } = query && query.Items[0]
+  const success = !timestamp || +new Date() - timestamp > waitMS
+  const response = success
+    ? await unlockDoor()
+    : `wait for ${waitMS - (+new Date() - timestamp)} ms`
   await dynamoDb
-    .put({ TableName, Item: { timestamp: +new Date(), success: body.success } })
+    .put({ TableName, Item: { timestamp: +new Date(), success: success + '' } })
     .promise()
 
   return {
     statusCode: 200,
-    body: JSON.stringify(body),
+    body: response,
   }
 }
 
