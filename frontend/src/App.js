8a1c9f112d930ecf32e4a3af303607fa849a7a95
anthager <anthage@student.chalmers.se>
2019-05-10 17:24:48 +0200

    add mailme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,34 @@ import React from 'react'
 import './App.css'
 import RequestHOC from './RequestHOC'
 
-const failMsg = `failed, please dont try again.<br/> <a href="mailto:hej@anton.pizza?subject=the door open shit is broken man&body=>:(" style="font-size:48px" >if you want, tell me it's broken</a>`
-const successMsg = `door opened, have a nice day mate :) <br/> <a href="mailto:hej@anton.pizza?subject=you good&body=<3" style="font-size:48px">If you want, tell me im good</a> `
+const failMsg = `failed, please dont try again.</a>`
+const successMsg = `door opened, have a nice day mate :)`
 
 function App() {
   return (
     <div className="App">
       <RequestHOC>
         {(openDoor, loading, res) => {
-          if (res) {
-            return <span className="center">{successMsg}</span>
+          if (res !== undefined) {
+            return (
+              <div className="center">
+                <span className="center">{res ? successMsg : failMsg}</span>
+                {res ? (
+                  <a className="mail center" href="mailto:hej@anton.pizza?subject=you good&body=<3">
+                    If you want, tell me im good
+                  </a>
+                ) : (
+                  <a
+                    href="mailto:hej@anton.pizza?subject=the door open shit is broken man&body=>:("
+                    style="font-size:48px"
+                  >
+                    if you want, tell me its broken{' '}
+                  </a>
+                )}
+              </div>
+            )
           } else if (loading) {
-            return <span className="center">Loading</span>
+            return <span className="center">Loading...</span>
           }
           return (
             <div className="cover-all clickable" onClick={openDoor}>
@@ -24,9 +40,6 @@ function App() {
       </RequestHOC>
     </div>
   )
-  // <div className="App clickable" onClick={openDoor}>
-  //   <span className="center">Click anywhere to unlock</span>
-  // </div>
 }
 
 export default App
