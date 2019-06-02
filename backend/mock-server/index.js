82220cc4e4b1785195c5f86c2697db68800884b7
anthager <anthage@student.chalmers.se>
2019-06-03 01:45:58 +0200

    move the files from serverless to backend and finished login route for mock server

--- backend/mock-server/index.js
+++ backend/mock-server/index.js
@@ -2,35 +2,48 @@ const express = require('express')
 
 const app = express()
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 1111
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
-  console.log('Press Ctrl+C to quit.')
 })
 
-app.get('/', (req, res) => {
-  console.log(req.headers)
-  res.set
-  res.status(200).send('hej')
+app.post('/wp-login.php', (req, res) => {
+  res.cookie('Fast2User_language', 'sv', { secure: true, httpOnly: true })
+  res.cookie('PHPSESSID', 'ut5h573hif2mneh2scl38radn7')
+  res.cookie('wordpress_test_cookie', 'WP+Cookie+check', { secure: true })
+  res.cookie('Fast2User_ssoId', 'V2-S2VJS2k3TnR3UTJtWG9Cd2ErUGczdz09', {
+    secure: true,
+    httpOnly: true,
+  })
+  res.cookie('Fast2User_ssoIdHash', 'qlprakGJfOCze6LS2O4K3REKqnydOCgCODod8Me1hgo', {
+    secure: true,
+    httpOnly: true,
+  })
+  res.cookie('Fast2User_timestamp', '20190211235702', { secure: true, httpOnly: true })
+  res.cookie('Fast2User_timestampHash', 'QVOV0GMiOtgLd2Bn-nAxmDB5OjGYYzp95Ft9Wy7hPBs', {
+    secure: true,
+    httpOnly: true,
+  })
+  res.cookie('f2in', 'f2-QVOV0GMiOtgLd2Bn-nAxmDB5OjGYYzp95Ft9Wy7hPBs-f2', { secure: true })
+  res.cookie(
+    'wordpress_sec_8af54e892ed77e6d1bb89eb523c33785',
+    '9606156157%7C1550098622%7C1tzzhstqCT8cyHxVzxMyqv7Rxu9Ehwcd030YnbmkXOU%7Ccc9b88ed08bbc280af56d2b8d6e7a2a10f2733d2c2f7e09fa402cb5d45e27b28',
+    { secure: true, httpOnly: true, path: '/wp-content/plugins' },
+  )
+  res.cookie(
+    'wordpress_sec_8af54e892ed77e6d1bb89eb523c33785',
+    '9606156157%7C1550098622%7C1tzzhstqCT8cyHxVzxMyqv7Rxu9Ehwcd030YnbmkXOU%7Ccc9b88ed08bbc280af56d2b8d6e7a2a10f2733d2c2f7e09fa402cb5d45e27b28',
+    { secure: true, httpOnly: true },
+  )
+  res.cookie(
+    'wordpress_logged_in_8af54e892ed77e6d1bb89eb523c33785',
+    '9606156157%7C1550098622%7C1tzzhstqCT8cyHxVzxMyqv7Rxu9Ehwcd030YnbmkXOU%7Caf17d64ea7b6551bf317dab206a1ca074c562dc25ce7910fc4914f1bef4dafd0',
+    { secure: true, httpOnly: true },
+  )
+  res.status(302).send('hej')
 })
 app.post('/', (req, res) => {
   console.log(req.headers)
 
-  res.set({
-    Date: 'Mon, 11 Feb 2019 12:15:34 GMT',
-    Server: 'Apache',
-    'Cache-Control': 'no-cache, must-revalidate, max-age=0',
-    Expires: 'Wed, 11 Jan 1984 05:00:00 GMT',
-    Pragma: 'no-cache',
-    'X-Content-Type-Options': 'nosniff',
-    'X-XSS-Protection': '1; mode=block',
-    'X-Frame-Options': 'SAMEORIGIN',
-    'Set-Cookie': 'wordpress_test_cookie=WP+Cookie+check; path=/; secure',
-    Location: 'https://www.chalmersstudentbostader.se/min-bostad/',
-    'Content-Length': '0',
-    'Keep-Alive': 'timeout=5, max=91',
-    Connection: 'Keep-Alive',
-    'Content-Type': 'text/html; charset=UTF-8',
-  })
-  res.status(200).send('hej')
+  res.status(302).send('hej')
 })
---
+++ backend/src/unlock-door/mocks.js
@@ -0,0 +1,23 @@
+const { logger } = require('../shared/utils')
+const mockUrlPayload = `jQuery({"html":{"alert":""},"data":{"aptuslogin@APTUSPORT":{"objekt":[{"objektNr":"KV7A1122 Kemivägen 7A","aptusUrl":"https://apt-www.chalmersstudentbostader.se/aptusportal/Account/RemoteLogin?module=Lock&customerName=KV7A1122&timestamp=2019-02-11 23:57:03&hash=WtIN3soStBBxPdEwt_2PRv8NZ34*"}]},"aptuslogin":{"objekt":[{"objektNr":"KV7A1122 Kemivägen 7A","aptusUrl":"https://apt-www.chalmersstudentbostader.se/aptusportal/Account/RemoteLogin?module=Booking&customerName=KV7A1122&timestamp=2019-02-11 23:57:03&hash=WtIN3soStBBxPdEwt_2PRv8NZ34*"}]}},"javascripts":[],"messages":[],"events":[],"openWindow":null,"redirectUrl":null,"replaceUrl":null,"CSRFtoken":"3c81320f-9d2d-463d-b950-69369b345807","fileResult":null});`
+
+const mockCookies =
+  'Fast2User_language=sv; path=/; secure; httponly, PHPSESSID=57898kdipe7031k8etc8hb95d0; path=/, wordpress_test_cookie=WP+Cookie+check; path=/; secure, Fast2User_ssoId=V2-S2VJS2k3TnR3UTJtWG9Cd2ErUGczdz09; path=/; secure; httponly, Fast2User_ssoIdHash=Nv0em1xe_4G63bMRPaI2dSD_DHuQsaVPD7Ka2GEbwv8; path=/; secure; httponly, Fast2User_timestamp=20190211233221; path=/; secure; httponly, Fast2User_timestampHash=gXvNrvDzDMdV34ka-psJ4tH2XdCTp67Px4orhKDhtfc; path=/; secure; httponly, f2in=f2-gXvNrvDzDMdV34ka-psJ4tH2XdCTp67Px4orhKDhtfc-f2; path=/; secure, wordpress_sec_8af54e892ed77e6d1bb89eb523c33785=9606156157%7C1550097141%7CSfdJw1ThL5JQU4IAaYuEIV5piLDM3w3e9Xkytdqwdj7%7C721eae4993f785106bb572e9a41491ef64accbd4a49fb49b21955a0de9510b32; path=/wp-content/plugins; secure; httponly, wordpress_sec_8af54e892ed77e6d1bb89eb523c33785=9606156157%7C1550097141%7CSfdJw1ThL5JQU4IAaYuEIV5piLDM3w3e9Xkytdqwdj7%7C721eae4993f785106bb572e9a41491ef64accbd4a49fb49b21955a0de9510b32; path=/; secure; httponly, wordpress_logged_in_8af54e892ed77e6d1bb89eb523c33785=9606156157%7C1550097141%7CSfdJw1ThL5JQU4IAaYuEIV5piLDM3w3e9Xkytdqwdj7%7C72d4603bf8c26bd8fe7d04929c86436b482405ed658c1fcd30d9319ea5d184a3; path=/; secure; httponly'
+
+const mockUrlBody = `body: jQuery({"html":{"alert":""},"data":{"aptuslogin@APTUSPORT":{"objekt":[{"objektNr":"KV7A1122 Kemivägen 7A","aptusUrl":"https://apt-www.chalmersstudentbostader.se/aptusportal/Account/RemoteLogin?module=Lock&customerName=KV7A1122&timestamp=2019-06-02 18:20:08&hash=5BygwAGsFxW7Nz6kxgCcQn4xStI*"}]},"aptuslogin":{"objekt":[{"objektNr":"KV7A1122 Kemivägen 7A","aptusUrl":"https://apt-www.chalmersstudentbostader.se/aptusportal/Account/RemoteLogin?module=Booking&customerName=KV7A1122&timestamp=2019-06-02 18:20:08&hash=5BygwAGsFxW7Nz6kxgCcQn4xStI*"}]}},"javascripts":[],"messages":[],"events":[],"openWindow":null,"redirectUrl":null,"replaceUrl":null,"CSRFtoken":"d1edf10d-71d9-44d2-8917-99cc652fbfbc","fileResult":null});
+fetched aptusUrl successfully...
+payload: jQuery({"html":{"alert":""},"data":{"aptuslogin@APTUSPORT":{"objekt":[{"objektNr":"KV7A1122 Kemivägen 7A","aptusUrl":"https://apt-www.chalmersstudentbostader.se/aptusportal/Account/RemoteLogin?module=Lock&customerName=KV7A1122&timestamp=2019-06-02 18:20:08&hash=5BygwAGsFxW7Nz6kxgCcQn4xStI*"}]},"aptuslogin":{"objekt":[{"objektNr":"KV7A1122 Kemivägen 7A","aptusUrl":"https://apt-www.chalmersstudentbostader.se/aptusportal/Account/RemoteLogin?module=Booking&customerName=KV7A1122&timestamp=2019-06-02 18:20:08&hash=5BygwAGsFxW7Nz6kxgCcQn4xStI*"}]}},"javascripts":[],"messages":[],"events":[],"openWindow":null,"redirectUrl":null,"replaceUrl":null,"CSRFtoken":"d1edf10d-71d9-44d2-8917-99cc652fbfbc","fileResult":null});
+url: www.chalmersstudentbostader.se/aptusportal/Account/RemoteLogin?module=Lock&customerName=KV7A1122&timestamp=2019-06-02 18:20:08&hash=5BygwAGsFxW7Nz6kxgCcQn4xStI*`
+
+const mockAptusCookies =
+  'ASP.NET_SessionId=1dxuv3ne3epk4b04ee4hi5bq; path=/; HttpOnly, .ASPXAUTH=2D3EE8712E8B377395AF7FE09DA34EEF2F89A1D0ED1EC039CCA27FD749CE3A48047665D209838CA499D535D1138F25FDB9A6FF92CA40F3A28614C706B87EF5D1AC9F03EA1CA1A0BDDCB2B969370528219A8A8C5250DEEA2762886F4451A0F531; path=/; HttpOnly'
+
+const unlockDoor = (doorID, delay = 2500) =>
+  new Promise(res => {
+    setTimeout(() => {
+      logger('mock unlock call')
+      res({ success: true })
+      return
+    }, delay)
+  })
+module.exports = { mockUrlPayload, mockCookies, mockUrlBody, mockAptusCookies, unlockDoor }
