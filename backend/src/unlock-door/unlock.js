483cfb20f46acdcec915d790e9ea27b9ec7b68ba
anthager <anthage@student.chalmers.se>
2019-06-05 01:44:05 +0200

    refactor unlock

--- backend/src/unlock-door/unlock.js
+++ backend/src/unlock-door/unlock.js
@@ -1,10 +1,19 @@
 const fetch = require('node-fetch')
 const { pwd, log } = require('./secrets')
 const { logger } = require('../shared/utils')
+const { PORT } = require('../../../mock-server/src/variables')
+
+const urls =
+  process.env.NODE_ENV === 'test'
+    ? { csb: `localhost:${PORT}`, aptus: `localhost:${PORT}` }
+    : {
+        csb: `https://www.chalmersstudentbostader.se`,
+        aptus: `https://apt-www.chalmersstudentbostader.se`,
+      }
 
 async function getCsbCookies() {
   let cookies
-  const res = await fetch('https://www.chalmersstudentbostader.se/wp-login.php', {
+  const res = await fetch(`${urls.csb}/wp-login.php`, {
     headers: {
       Accept:
         'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -32,7 +41,7 @@ async function getCsbCookies() {
 
 async function getUrlToAptus(cookies) {
   const body = await (await fetch(
-    'https://www.chalmersstudentbostader.se/widgets/?callback=jQuery&widgets%5B%5D=aptuslogin%40APTUSPORT',
+    `${urls.csb}/widgets/?callback=jQuery&widgets%5B%5D=aptuslogin%40APTUSPORT`,
     {
       credentials: 'include',
       headers: {
@@ -51,9 +60,8 @@ async function getUrlToAptus(cookies) {
       method: 'GET',
     },
   )).text()
-  console.log('body:', body)
   logger('fetched aptusUrl successfully...')
-  return formatUrl(getUrlFromPayload(body))
+  return getAptusUrlFromPayload(body)
 }
 
 async function getAptusCookies(url) {
@@ -79,27 +87,24 @@ async function getAptusCookies(url) {
 }
 
 async function _unlockDoor(cookie, doorID) {
-  return (await fetch(
-    `https://apt-www.chalmersstudentbostader.se/AptusPortal/Lock/UnlockEntryDoor/${doorID}`,
-    {
-      credentials: 'include',
-      headers: {
-        accept: '*/*',
-        'accept-language': 'sv,sv-SE;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cache-control': 'no-cache',
-        pragma: 'no-cache',
-        'x-requested-with': 'XMLHttpRequest',
-        referer: 'https://apt-www.chalmersstudentbostader.se/AptusPortal/Lock',
-        'User-Agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-        cookie,
-      },
-      referrer: 'https://apt-www.chalmersstudentbostader.se/AptusPortal/Lock',
-      referrerPolicy: 'no-referrer-when-downgrade',
-      method: 'GET',
-      mode: 'cors',
+  return (await fetch(`${urls.aptus}/AptusPortal/Lock/UnlockEntryDoor/${doorID}`, {
+    credentials: 'include',
+    headers: {
+      accept: '*/*',
+      'accept-language': 'sv,sv-SE;q=0.9,en-US;q=0.8,en;q=0.7',
+      'cache-control': 'no-cache',
+      pragma: 'no-cache',
+      'x-requested-with': 'XMLHttpRequest',
+      referer: 'https://apt-www.chalmersstudentbostader.se/AptusPortal/Lock',
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+      cookie,
     },
-  )).text()
+    referrer: 'https://apt-www.chalmersstudentbostader.se/AptusPortal/Lock',
+    referrerPolicy: 'no-referrer-when-downgrade',
+    method: 'GET',
+    mode: 'cors',
+  })).text()
 }
 
 function parseCookies(cookies) {
@@ -110,19 +115,11 @@ function parseCookies(cookies) {
     .reduce((acc, val) => `${acc}; ${val}`)
 }
 
-function getUrlFromPayload(payload) {
-  console.log('payload:', payload)
-  const reg = new RegExp(
-    // eslint-disable-next-line prettier/prettier
-    'www.chalmersstudentbostader.se/aptusportal/Account/RemoteLogin\\?module=Lock&.+?(?="}]},)',
-  )
-  return reg.exec(payload).toString()
-}
-
-function formatUrl(url) {
-  console.log('url:', url)
-  const arr = url.split(' ')
-  return `https://apt-${arr[0]}%20${arr[1]}`
+function getAptusUrlFromPayload(payload) {
+  const urlParts = JSON.parse(payload.substring(7, payload.length - 2)).data[
+    'aptuslogin@APTUSPORT'
+  ].objekt[0].aptusUrl.split(' ')
+  return `${urlParts[0]}%20${urlParts[1]}`
 }
 
 async function unlockDoor(doorID) {
@@ -141,5 +138,5 @@ async function unlockDoor(doorID) {
     return { success: false }
   }
 }
-unlockDoor(116402)
-module.exports = { parseCookies, unlockDoor }
+// unlockDoor(116402)
+module.exports = { parseCookies, unlockDoor, getAptusUrlFromPayload }
