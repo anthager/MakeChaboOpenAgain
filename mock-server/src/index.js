c0e89c5a836a0b5ff749b0390b565646a0fd77ae
anthager <anthage@student.chalmers.se>
2019-06-07 00:14:06 +0200

    fix some misstakes in mock cookies

--- mock-server/src/index.js
+++ mock-server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
+const { logger } = require('../../backend/src/shared/utils')
 const { PORT } = require('./variables')
 
 const app = express()
@@ -55,28 +56,31 @@ app.post('/wp-login.php', (req, res) => {
  */
 app.get('/widgets', (req, res) => {
   const correctCookies = {
-    PHPSESSID: 'mahodvd5ct6ccb8s1foghfjkd6',
-    __utmc: '77924038',
-    wordpress_test_cookie: 'WP+Cookie+check',
-    __utmz: '77924038.1556802487.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
-    PLAY_SESSION:
-      '2331d5852285a7e60d1892b7c8b6951eb5853951-Fast2User_session=V2-S2VJS2k3TnR3UTJtWG9Cd2ErUGczdz09&uuid=c1f1de3f-d708-4380-8953-ef611528a9a1',
-    __utma: '77924038.1281765984.1556802487.1559196793.1559489289.16',
-    __utmt: '1',
-    f2in: 'f2-SM3GUaZSYNJlsnZvNpEeV1ltPxPNTM4fJCAi7dEB8X4-f2',
     Fast2User_language: 'sv',
-    __utmb: '77924038.4.10.1559489289',
+    PHPSESSID: 'ut5h573hif2mneh2scl38radn7',
+    wordpress_test_cookie: 'WP+Cookie+check',
+    Fast2User_ssoId: 'V2-S2VJS2k3TnR3UTJtWG9Cd2ErUGczdz09',
+    Fast2User_ssoIdHash: 'qlprakGJfOCze6LS2O4K3REKqnydOCgCODod8Me1hgo',
+    Fast2User_timestamp: '20190211235702',
+    Fast2User_timestampHash: 'QVOV0GMiOtgLd2Bn-nAxmDB5OjGYYzp95Ft9Wy7hPBs',
+    f2in: 'f2-QVOV0GMiOtgLd2Bn-nAxmDB5OjGYYzp95Ft9Wy7hPBs-f2',
+    wordpress_sec_8af54e892ed77e6d1bb89eb523c33785:
+      '9606156157%7C1550098622%7C1tzzhstqCT8cyHxVzxMyqv7Rxu9Ehwcd030YnbmkXOU%7Ccc9b88ed08bbc280af56d2b8d6e7a2a10f2733d2c2f7e09fa402cb5d45e27b28',
+    wordpress_logged_in_8af54e892ed77e6d1bb89eb523c33785:
+      '9606156157%7C1550098622%7C1tzzhstqCT8cyHxVzxMyqv7Rxu9Ehwcd030YnbmkXOU%7Caf17d64ea7b6551bf317dab206a1ca074c562dc25ce7910fc4914f1bef4dafd0',
   }
   const correctQuery = { callback: 'jQuery', widgets: ['aptuslogin@APTUSPORT'] }
   const badCookie = Object.entries(req.cookies).find(([key, val]) => correctCookies[key] !== val)
   const invalidQuery = JSON.stringify(req.query) !== JSON.stringify(correctQuery)
   if (badCookie) {
+    logger('bad cookies')
     return res.status(400).json({
       msg: 'bad cookie',
       expected: `${badCookie[0]}: ${correctCookies[badCookie[0]]}`,
       got: `${badCookie[0]}: ${badCookie[1]}`,
     })
   } else if (invalidQuery) {
+    logger('bad cookies')
     return res.status(400).json({
       msg: 'bad query',
       expected: JSON.stringify(correctQuery),
@@ -85,15 +89,17 @@ app.get('/widgets', (req, res) => {
   } else {
     return res.status(302).send(
       `jQuery({"html":{"alert":""},"data":{"aptuslogin@APTUSPORT":{"objekt":[{"objektNr":"KV7A1122 Kemivägen 7A","aptusUrl":
-        "http://localhost:${PORT}/aptusportal/Account/RemoteLogin?module=Lock&customerName=KV7A1122&timestamp=2019-06-02 18:20:08&
-        hash=5BygwAGsFxW7Nz6kxgCcQn4xStI*"}]},"aptuslogin":{"objekt":[{"objektNr":"KV7A1122 Kemivägen 7A","aptusUrl":
-        "https://apt-www.chalmersstudentbostader.se/aptusportal/Account/RemoteLogin?module=Booking&customerName=KV7A1122&timestamp=2019-06-02 18:20:08&
-        hash=5BygwAGsFxW7Nz6kxgCcQn4xStI*"}]}},"javascripts":[],"messages":[],"events":[],"openWindow":null,"redirectUrl":null,"replaceUrl":null,"CSRFtoken":
-        "d1edf10d-71d9-44d2-8917-99cc652fbfbc","fileResult":null});`,
+        "http://localhost:${PORT}/aptusportal/Account/RemoteLogin?module=Lock&customerName=KV7A1122&timestamp=2019-06-02 18:20:08&hash=5BygwAGsFxW7Nz6kxgCcQn4xStI*"}]},
+        "aptuslogin":{"objekt":[{"objektNr":"KV7A1122 Kemivägen 7A","aptusUrl":
+        "http://localhost:${PORT}/aptusportal/Account/RemoteLogin?module=Booking&customerName=KV7A1122&timestamp=2019-06-02 18:20:08&hash=5BygwAGsFxW7Nz6kxgCcQn4xStI*"}]}},"javascripts":[],
+        "messages":[],"events":[],"openWindow":null,"redirectUrl":null,"replaceUrl":null,"CSRFtoken":"d1edf10d-71d9-44d2-8917-99cc652fbfbc","fileResult":null});`,
     )
   }
 })
 
+/**
+ * get aptus cookies
+ */
 app.get('/aptusportal/Account/RemoteLogin', (req, res) => {
   const correctQuery = {
     module: 'Lock',
@@ -112,22 +118,24 @@ app.get('/aptusportal/Account/RemoteLogin', (req, res) => {
     res.cookie('ASP.NET_SessionId', '1dxuv3ne3epk4b04ee4hi5bq', { httpOnly: true })
     res.cookie(
       '.ASPXAUTH',
-      `2D3EE8712E8B377395AF7FE09DA34EEF2F89A1D0ED1EC039CCA27FD749CE3A48047665D209838CA499D535D1138F25
-      FDB9A6FF92CA40F3A28614C706B87EF5D1AC9F03EA1CA1A0BDDCB2B969370528219A8A8C5250DEEA2762886F4451A0F531`,
+      `2D3EE8712E8B377395AF7FE09DA34EEF2F89A1D0ED1EC039CCA27FD749CE3A48047665D209838CA499D535D1138F25FDB9A6FF92CA40F3A28614C706B87EF5D1AC9F03EA1CA1A0BDDCB2B969370528219A8A8C5250DEEA2762886F4451A0F531`,
       { httpOnly: true },
     )
     return res.status(302).send('hej')
   }
 })
 
+/**
+ * unlock the door
+ */
 app.get('/AptusPortal/Lock/UnlockEntryDoor/116402', (req, res) => {
   const correctCookies = {
-    '.ASPXAUTH': `2D3EE8712E8B377395AF7FE09DA34EEF2F89A1D0ED1EC039CCA27FD749CE3A48047665D209838CA499D535D1138F25
-    FDB9A6FF92CA40F3A28614C706B87EF5D1AC9F03EA1CA1A0BDDCB2B969370528219A8A8C5250DEEA2762886F4451A0F531`,
+    '.ASPXAUTH': `2D3EE8712E8B377395AF7FE09DA34EEF2F89A1D0ED1EC039CCA27FD749CE3A48047665D209838CA499D535D1138F25FDB9A6FF92CA40F3A28614C706B87EF5D1AC9F03EA1CA1A0BDDCB2B969370528219A8A8C5250DEEA2762886F4451A0F531`,
     'ASP.NET_SessionId': '1dxuv3ne3epk4b04ee4hi5bq',
   }
   const badCookie = Object.entries(req.cookies).find(([key, val]) => correctCookies[key] !== val)
   if (badCookie) {
+    logger('bad cookie')
     return res.status(400).json({
       msg: 'bad cookie',
       expected: `${badCookie[0]}: ${correctCookies[badCookie[0]]}`,
