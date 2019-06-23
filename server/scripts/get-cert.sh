0e7f5f089c33a6f665ef13470b67c6becb9329c6
anthager <anthage@student.chalmers.se>
2019-06-23 14:32:04 +0200

    update all server names and certscript to beta.open.anton.pizza for dev

--- server/scripts/get-cert.sh
+++ server/scripts/get-cert.sh
@@ -8,4 +8,7 @@ certbot/certbot certonly \
 --webroot \
 --webroot-path /var/www/certbot \
 --email hej@anton.pizza \
--d "tst.anton.pizza"
+-d "beta.open.anton.pizza"
+-d "beta.api.open.anton.pizza"
+-d "beta.staging.open.anton.pizza"
+-d "beta.staging.api.anton.pizza"
