c4526cdb522fe93d2a46530a360a733a45f2f746
anthager <anthage@student.chalmers.se>
2019-06-23 14:45:45 +0200

    fix wrong domain in cert script

--- server/scripts/get-cert.sh
+++ server/scripts/get-cert.sh
@@ -8,7 +8,7 @@ certbot/certbot certonly \
 --webroot \
 --webroot-path /var/www/certbot \
 --email hej@anton.pizza \
--d "beta.open.anton.pizza"
--d "beta.api.open.anton.pizza"
--d "beta.staging.open.anton.pizza"
--d "beta.staging.api.anton.pizza"
+-d "beta.open.anton.pizza" \
+-d "beta.api.open.anton.pizza" \
+-d "beta.staging.open.anton.pizza" \
+-d "beta.staging.api.open.anton.pizza" \
