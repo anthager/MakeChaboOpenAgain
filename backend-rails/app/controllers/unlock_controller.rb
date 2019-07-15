ca8a4ff949e9d300b3bc6dbdff7e2b228f69613d
anthager <anthage@student.chalmers.se>
2019-07-16 00:48:54 +0200

    add unlock controller

---
+++ backend-rails/app/controllers/unlock_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class UnlockController < ApplicationController
+  def unlock
+    UnlockService.new('http://localhost:1111', 'http://localhost:1111', 'nice', 'nice').unlock_door
+    success, wait_time = enough_time_has_passed_since_last_opening?(8)
+    Opening.create(success: success)
+    render json: { success: success, wait_time: wait_time }, status: :ok
+    response.headers['Access-Control-Allow-Credentials'] = true
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
+  def enough_time_has_passed_since_last_opening?(threshold)
+    latest = Opening.maximum('created_at')
+    time_since_latest = Time.now - latest
+    return false, threshold - time_since_latest if time_since_latest < threshold
+
+    true
+  end
+end
