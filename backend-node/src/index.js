636511872ce26a469aeafef401f1a252ec02f951
anthager <anthage@student.chalmers.se>
2019-07-26 01:11:52 +0200

    throw when env is not provided

--- backend-node/src/index.js
+++ backend-node/src/index.js
@@ -2,14 +2,32 @@ const express = require('express')
 const dotenv = require('dotenv')
 const router = require('./router')
 
-dotenv.config()
-
 const app = express()
-app.use(router)
 const PORT = process.env.PORT || 8080
+
+dotenv.config()
+if (!process.env.PWD) {
+  throw new Error('No "PWD" provided')
+}
+
+if (!process.env.LOG) {
+  throw new Error('No "LOG" provided')
+}
+
+if (!process.env.APTUS_URL) {
+  throw new Error('No "APTUS_URL" provided')
+}
+
+if (!process.env.CSB_URL) {
+  throw new Error('No "CSB_URL" provided')
+}
+
+app.use(router)
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
+  console.log(`using aptus url: ${process.env.APTUS_URL}`)
+  console.log(`using csb url: ${process.env.CSB_URL}`)
 })
 
 module.exports = app
