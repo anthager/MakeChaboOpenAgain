910f7379eed79dc4072ce7c7af27ee20aeea285e
anthager <anthage@student.chalmers.se>
2019-07-27 13:44:10 +0200

    add config script for handling config with ease

---
+++ backend-node/src/config.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const defaultConfig = {
+  PASSWORD: 'nice',
+  LOG: 'nice',
+  CSB_URL: 'http://localhost:1111',
+  APTUS_URL: 'http://localhost:1111',
+  THRESHOLD: 8000,
+}
+const configs = {
+  test: {},
+  development: {},
+  staging: {},
+  production: {},
+}
+
+const config = {}
+function load() {
+  console.log('run load')
+  Object.entries(defaultConfig).map(([key, value]) => {
+    config[key] = process.env[key] || configs[process.env.NODE_ENV][key] || value
+  })
+}
+
+module.exports = { config, load }
