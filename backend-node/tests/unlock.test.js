6eae962fadbbfca77246f5db40bb6ef746cabe90
anthager <anthage@student.chalmers.se>
2019-07-28 17:44:37 +0200

    add integration test

--- backend-node/tests/unlock.test.js
+++ backend-node/tests/unlock.test.js
@@ -1,14 +1,30 @@
 const knex = require('../knexfile')
-const { getTimeOfLastSuccessful } = require('../src/services/unlock.service')
+const request = require('supertest')
+const app = require('../src/index')
 
 describe('request testing', () => {
   beforeAll(async () => {
     await knex.migrate.latest()
     await knex.seed.run()
   })
-  it('latest should be a date', async () => {
-    const latest = await getTimeOfLastSuccessful()
-    expect(+new Date(latest)).toBeGreaterThan(0)
+  it('should succeed when hitting /unlock after fresh', async () => {
+    const res = (await request(app)
+      .get('/unlock')
+      .expect(200)).res
+
+    const resBody = JSON.parse(res.text)
+    expect(resBody).toHaveProperty('success')
+    expect(resBody.success).toEqual(true)
+  })
+  it('should fail when hitting /unlock again', async () => {
+    const res = (await request(app)
+      .get('/unlock')
+      .expect(200)).res
+    const resBody = JSON.parse(res.text)
+    expect(resBody).toHaveProperty('success')
+    expect(resBody).toHaveProperty('wait')
+    expect(resBody.success).toEqual(false)
+    expect(resBody.wait).toBeGreaterThan(0)
   })
   afterAll(async () => {
     // you're my wonderwall
