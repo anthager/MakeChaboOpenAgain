a0911534e8dbb9b8a4fb6dd4db7ac1535f1d5132
anthager <anthage@student.chalmers.se>
2019-07-28 17:44:21 +0200

    test seed only deletes db

--- backend-node/db/seeds-test/01_unlocks.js
+++ backend-node/db/seeds-test/01_unlocks.js
@@ -1,8 +1,3 @@
 exports.seed = async knex => {
   await knex('unlocks').del()
-  await knex('unlocks').insert([
-    { id: 1, success: true },
-    { id: 2, success: false },
-    { id: 3, success: true },
-  ])
 }
