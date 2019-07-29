fd475a27028a711090a00b62b0a7297fc72ed80f
anthager <anthage@student.chalmers.se>
2019-07-29 22:56:21 +0200

    remove backend-rails

--- backend-rails/app/models/opening.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Opening < ApplicationRecord
-  validates :success, inclusion: { in: [true, false] }
-end
--- backend-rails/db/migrate/20190707230700_create_openings.rb
+++
@@ -1,9 +0,0 @@
-class CreateOpenings < ActiveRecord::Migration[5.2]
-  def change
-    create_table :openings do |t|
-      t.boolean :success
-
-      t.timestamps
-    end
-  end
-end
