4dc52d34f027c7df9086c40e3e59e8d017decb9d
anthager <anthage@student.chalmers.se>
2019-08-08 21:58:19 +0200

    add check that the db is up before the server is started

--- backend-node/src/index.js
+++ backend-node/src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = require('./router')
 const configService = require('./config')
+const knex = require('../db/knex')
+const reconnectInterval = 5000
+
 configService.load()
 const config = configService.config
 
@@ -9,13 +12,32 @@ const PORT = process.env.PORT || 8080
 
 app.use(router)
 app.get('/', (req, res) => {
-  res.send(200).json('nice')
+  res.status(200).json('Quite alright thanks')
 })
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-    console.log(`using config:\n`, config)
+  ;(async () => {
+    await isDatabaseUp()
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}`)
+      console.log(`using config:\n`, config)
+    })
+  })()
+}
+
+async function isDatabaseUp() {
+  return new Promise(async res => {
+    try {
+      console.log('attempting to connect to database...')
+      await knex.raw('SELECT * FROM unlocks;')
+      console.log('connected to database successfully')
+      return res('nice')
+    } catch (err) {
+      console.log(
+        `connecting to database failed, attempting to reconnect in ${reconnectInterval}ms...`,
+      )
+      setTimeout(isDatabaseUp, reconnectInterval)
+    }
   })
 }
 
