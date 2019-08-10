9bf0d7b7314697319d23a9aef9e68da4204a6541
anthager <anthage@student.chalmers.se>
2019-08-10 12:26:21 +0200

    add start-db-staging script

---
+++ server/scripts/start-db-staging.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+docker run -p 5432:5432 --env-file=/var/.envs/.db_staging.env --name mcoa_db_staging --restart always -v pgdata:/var/lib/postgresql/data -d postgres:10.5
