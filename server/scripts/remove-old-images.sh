07a540f74f5cceea2a9c5ef83ab70d77edea0670
anthager <anthage@student.chalmers.se>
2019-08-22 17:00:29 +0200

    make remove-old-images executable and only run docker rmi if there actually are images to be removed

--- server/scripts/remove-old-images.sh
+++ server/scripts/remove-old-images.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 set -e
-comm -23 <(docker images | awk '{print $1":"$2}' | grep "antonhager" | sort) <(docker ps | awk '{print $2}' | tail -n +2 | sort) | xargs docker rmi
+images_to_remove=$(comm -23 <(docker images | awk '{print $1":"$2}' | grep "antonhager" | sort) <(docker ps | awk '{print $2}' | tail -n +2 | sort))
+
+for image in $images_to_remove 
+do
+	docker rmi $image
+done
