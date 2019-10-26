51e6c46af71fdd02d74a3d1e0ab299152c1ed24a
anthager <anthage@student.chalmers.se>
2019-10-26 16:53:00 +0200

    update ci and add deploy scripts to make ci yml more readable

--- scripts/ci-scripts/deploy-alohomora.sh
+++ scripts/ci-scripts/deploy-alohomora.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 STAGE=$1
 DOCKER_USERNAME=$2
--- scripts/ci-scripts/deploy-mock-server.sh
+++ scripts/ci-scripts/deploy-mock-server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 #!/usr/bin/env bash
