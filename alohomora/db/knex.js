52f3fc5419d1338e7013b9c46b662df2b3ace5b1
anthager <anthage@student.chalmers.se>
2019-10-27 21:47:15 +0100

    update knex to use connection string

--- alohomora/db/knex.js
+++ alohomora/db/knex.js
@@ -1,1 +1,15 @@
-module.exports = require('knex')(require('./knexfile')[process.env.NODE_ENV])
+module.exports = require('knex')({
+  client: 'postgresql',
+  connection: process.env.CONNECTION_STRING,
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: 'db/migrations',
+  },
+  seeds: {
+    directory: 'db/seeds',
+  },
+})
