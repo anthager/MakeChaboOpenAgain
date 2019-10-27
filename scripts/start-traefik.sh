3582e44daf836b746757ba43e6d4247a44fefb90
anthager <anthage@student.chalmers.se>
2019-10-27 21:33:21 +0100

    add traefik.yml

---
+++ scripts/start-traefik.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+
+docker run -d \
+	-p 80:80 \
+	-p 443:443 \
+	-p 8080:8080 \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v /var/traefik.yml:/etc/traefik/traefik.yml \
+	-v /var/acme.json:/acme.json \
+	--name traefik \
+	--restart always \
+	traefik
